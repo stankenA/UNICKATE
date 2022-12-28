99ce863ee0e720875ad86ef5973da6ea9a01062c
stanken_a <artyemst3@mail.ru>
2022-12-28 19:44:02 +0300

    feat: complete course page (desktop)

---
+++ scripts/course.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".feedback__swiper-1", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".feedback__btn-next-1",
+    prevEl: ".feedback__btn-prev-1",
+  },
+});
+
+var swiper = new Swiper(".feedback__swiper-2", {
+  slidesPerView: 1,
+  spaceBetween: 100,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".feedback__btn-next-2",
+    prevEl: ".feedback__btn-prev-2",
+  },
+});
+
